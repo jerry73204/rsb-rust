efbab0a4095d806629bda801fc3f8bba3a8b285e
aeon <jerry73204@gmail>
2022-02-19 15:39:57 +0800

    Change codegen options

--- librsb-sys/build.rs
+++ librsb-sys/build.rs
@@ -1,6 +1,4 @@
-use anyhow::anyhow;
-use anyhow::bail;
-use anyhow::Result;
+use anyhow::{anyhow, bail, Result};
 use once_cell::sync::Lazy;
 use std::{
     borrow::Cow,
@@ -30,7 +28,7 @@ fn main() -> Result<()> {
         #[cfg(target_os = "linux")]
         {
             #[cfg(feature = "codegen")]
-            codegen()?;
+            codegen::codegen()?;
 
             link()?;
         }
@@ -45,31 +43,58 @@ fn main() -> Result<()> {
 }
 
 #[cfg(feature = "codegen")]
-fn codegen() -> Result<()> {
-    const BINDINGS_FILE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/src/bindings.rs");
+mod codegen {
+    use super::*;
 
-    let include_dir = probe_include_dir().ok_or_else(|| anyhow!("unable to find include dir"))?;
-    let header_files = ["rsb.h", "blas_sparse.h", "rsb_types.h", "rsb-config.h"];
+    use bindgen::{AliasVariation, EnumVariation};
 
-    // // Tell cargo to invalidate the built crate whenever the wrapper changes
+    pub fn codegen() -> Result<()> {
+        const BINDINGS_FILE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/src/bindings.rs");
 
-    let mut build = bindgen::Builder::default();
+        let include_dir =
+            probe_include_dir().ok_or_else(|| anyhow!("unable to find include dir"))?;
+        let header_files = ["rsb.h", "blas_sparse.h", "rsb_types.h", "rsb-config.h"];
 
-    for file in header_files {
-        let path = format!("{}/{}", include_dir.display(), file);
-        println!("cargo:rerun-if-changed={}", path);
-        build = build.header(path);
+        // Tell cargo to invalidate the built crate whenever the wrapper changes
+
+        let mut build = bindgen::Builder::default();
+
+        for file in header_files {
+            let path = format!("{}/{}", include_dir.display(), file);
+            println!("cargo:rerun-if-changed={}", path);
+            build = build.header(path);
+        }
+
+        let build = build
+            .derive_partialeq(true)
+            .derive_eq(true)
+            .derive_hash(true)
+            .allowlist_type("blas_.*")
+            .allowlist_type("rsb_.*")
+            .allowlist_function("BLAS_.*")
+            .allowlist_function("blas_.*")
+            .allowlist_function("rsb_.*")
+            .allowlist_var("BLAS_.*")
+            .allowlist_var("RSB_.*")
+            .default_enum_style(EnumVariation::Rust {
+                non_exhaustive: false,
+            })
+            .newtype_enum("blas_handle_type")
+            .new_type_alias("rsb_err_t")
+            .new_type_alias("rsb_type_t")
+            .new_type_alias("rsb_trans_t")
+            .new_type_alias("blas_sparse_matrix");
+
+        let bindings = build
+            .generate()
+            .map_err(|_| anyhow!("Unable to generate bindings"))?;
+
+        bindings
+            .write_to_file(BINDINGS_FILE)
+            .map_err(|_| anyhow!("Couldn't write bindings!"))?;
+
+        Ok(())
     }
-
-    let bindings = build
-        .generate()
-        .map_err(|_| anyhow!("Unable to generate bindings"))?;
-
-    bindings
-        .write_to_file(BINDINGS_FILE)
-        .map_err(|_| anyhow!("Couldn't write bindings!"))?;
-
-    Ok(())
 }
 
 fn link() -> Result<()> {
@@ -78,13 +103,6 @@ fn link() -> Result<()> {
     println!("cargo:rustc-link-lib=dylib=rsb");
     Ok(())
 }
-
-// fn probe_library_linux() -> Result<Library> {
-//     let include = probe_include_dir().ok_or_else(|| anyhow!("unable to find include dir"))?;
-//     let library = probe_library_dir().ok_or_else(|| anyhow!("unable to find library dir"))?;
-
-//     Ok(Library { include, library })
-// }
 
 fn probe_include_dir() -> Option<Cow<'static, Path>> {
     let path: Option<Cow<'_, Path>> = LIBRSB_INCLUDE
